e4fc7810f9826b9d3a99b2158ecc3a49c53ab580
Brun0K9 <133142934+Brun0K9@users.noreply.github.com>
2023-05-14 00:24:00 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+//-------------------------------------------------------------------//
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemButaoAlterar = document.querySelector(".imagem-botao")
+
+ 
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroAtivo = body.classList.contains("modo-escuro")
+    
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscuroAtivo) {  
+        imagemButaoAlterar.setAttribute("src", "./src/imagens/sun.png")
+    }
+    else {
+    imagemButaoAlterar.setAttribute("src", "./src/imagens/moon.png")
+    }
+});
+
+//-------------------------------------------------------------------//
+
+
